0d55bb7be3b56851bdc551567fb664cf313697ed
YuutaW <17158086+Trumeet@users.noreply.github.com>
2019-03-11 18:31:14 -0700

    feat: add logs in the script

--- install_extra_tools.sh
+++ install_extra_tools.sh
@@ -2,16 +2,20 @@
 # To run_install extra tools to build custom ROMs.
 
 run_install() {
-	echo "Install extra tools..."
+	echo "[Script] Install extra tools..."
 	apt-get install -y bc imagemagick ccache
 }
 
 # For automated builds, "auto" argument will be passed.
 if [ "$1" = "auto" ]
 then
+	echo "[Script] Detecting env"
+	echo "[Script] Docker tag: $DOCKER_TAG"
 	if [ "$DOCKER_TAG" = "extra-latest" ]
 	then
 		run_install
+	else
+		echo "[Script] Skipping"
 	fi
 else
 	run_install
